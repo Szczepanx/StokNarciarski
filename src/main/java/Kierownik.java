778f14359d384438b60149845ee8d034038987a3
Szczepan Gizicki <szczepangizicki@gmail.com>
2021-06-09 22:34:08 +0200

    Revert "NotComplited"

--- src/main/java/Kierownik.java
+++ src/main/java/Kierownik.java
@@ -8,59 +8,40 @@ public class Kierownik extends Pracownik{
         super(idPracownika, imiePracownika, nazwiskoPracownika);
     }
 
-    public Kierownik() {
-    }
-
     public double UstalNowaCeneWypozyczeniaSprzetu(){
         Scanner scanner = new Scanner(System.in);
         return scanner.nextDouble();
     }
     public List<Sprzet> DodajNowySprzet(List<Sprzet> listaSprzetow){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("ID sprzetu:");
         int idSprzetu = scanner.nextInt();
-        System.out.println("Producent sprzetu:");
-        String producent = scanner.next();
-        System.out.println("Cena sprzetu:");
+        String producent = scanner.nextLine();
         double cenaSprzetu = UstalNowaCeneWypozyczeniaSprzetu();
-        System.out.println("Typ sprzetu:");
-        String typSprzetu = scanner.next();
-        System.out.println("Dostepnosc sprzetu (true,false):");
+        String typSprzetu = scanner.nextLine();
         boolean dostepnoscSprzetu = scanner.nextBoolean();
         listaSprzetow.add(new Sprzet(idSprzetu,producent,cenaSprzetu,typSprzetu,dostepnoscSprzetu));
-        System.out.println(listaSprzetow);
         return listaSprzetow;
     }
     public List<Sprzet> UsunSprzet(List<Sprzet>listaSprzetow){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj nr z listy :");
         int idDoUsuniecia = scanner.nextInt();
-        listaSprzetow.remove(idDoUsuniecia-1);
-        System.out.println(listaSprzetow);
+        listaSprzetow.remove(idDoUsuniecia+1);
         return listaSprzetow;
     }
     public List<Instruktor> DodajNowegoInstruktora(List<Instruktor> listaInstruktorow){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Id instruktora:");
         int idInstruktora = scanner.nextInt();
-        System.out.println("Imie:");
-        String imieInstruktora = scanner.next();
-        System.out.println("Nazwsko:");
-        String nazwiskoInstruktora = scanner.next();
-        System.out.println("Poziom zaawansowania:");
-        String poziomZaawansowania = scanner.next();
-        System.out.println("Typ instruktora :");
-        String typInstruktora = scanner.next();
+        String imieInstruktora = scanner.nextLine();
+        String nazwiskoInstruktora = scanner.nextLine();
+        String poziomZaawansowania = scanner.nextLine();
+        String typInstruktora = scanner.nextLine();
         listaInstruktorow.add(new Instruktor(idInstruktora,imieInstruktora,nazwiskoInstruktora,poziomZaawansowania, typInstruktora,UstalCeneLekcjiInstruktora()));
-        System.out.println(listaInstruktorow);
         return listaInstruktorow;
     }
     public List<Instruktor> UsunInstruktora(List<Instruktor> listaInstruktorow){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Nr instruktora do usuniecia :");
         int idistruktoraDoUsuniecia = scanner.nextInt();
-        listaInstruktorow.remove(idistruktoraDoUsuniecia-1);
-        System.out.println(listaInstruktorow);
+        listaInstruktorow.remove(idistruktoraDoUsuniecia+1);
         return listaInstruktorow;
     }
     public double UstalCeneLekcjiInstruktora(){
@@ -70,38 +51,30 @@ public class Kierownik extends Pracownik{
     }
     public List<Pracownik> DodajPracownika(List<Pracownik> listaPracownikow){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Id pracownika :");
         int idPracownika = scanner.nextInt();
-        System.out.println("Imie:");
-        String imiePracownika = scanner.next();
-        System.out.println("Nazwisko:");
-        String nazwiskoPracownika= scanner.next();
+        String imiePracownika = scanner.nextLine();
+        String nazwiskoPracownika= scanner.nextLine();
 
         listaPracownikow.add(new Pracownik(idPracownika,imiePracownika,nazwiskoPracownika));
-        System.out.println(listaPracownikow);
         return listaPracownikow;
 
 
     }
     public List<Pracownik> UsunPracownika(List<Pracownik> listaPracownikow){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Nr pracownika do usuniecia:");
         int idPracownikaDoUsuniecia = scanner.nextInt();
-        listaPracownikow.remove(idPracownikaDoUsuniecia-1);
-        System.out.println(listaPracownikow);
+        listaPracownikow.remove(idPracownikaDoUsuniecia+1);
         return listaPracownikow;
 
     }
     public double UstalCeneZaNaliczanyCzas(){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Wpisz cene :");
         double cenaZaCzas = scanner.nextDouble();
         return cenaZaCzas;
 
     }
     public double UstalCeneZaNaliczonePunkty(){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Wpisz cene :");
         double cenaZaPunkty = scanner.nextDouble();
         return cenaZaPunkty;
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,77 +1,35 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-
-        Kierownik kierownik = new Kierownik();
-        List<Instruktor> listaInstruktorow = new LinkedList<>();
-
-        List<Pracownik> listaPracownikow = new LinkedList<>();
+        List<Pracownik> listaPracownikow = new ArrayList<>();
         listaPracownikow.add(new Pracownik(1,"Jan","Kowalski"));
         listaPracownikow.add(new Pracownik(2,"Andrzej","Nowak"));
         listaPracownikow.add(new Pracownik(3,"Tadeusz","Kaleta"));
         listaPracownikow.add(new Pracownik(4,"Stefan","Drab"));
 
-
-        
-
         List<Kierownik> listaKierowników = new ArrayList<>();
         listaKierowników.add(new Kierownik(5,"Janusz","Bela"));
-      
-        List<Sprzet> listaSprzetow = new LinkedList<>();
+
+        List<Sprzet> listaSprzetow = new ArrayList<>();
         listaSprzetow.add(new Sprzet(1,"Salomon",29.99,"Kask",true));
-        listaSprzetow.add(new Sprzet(2,"Salomon",50.00,"Narty",true));
-        listaSprzetow.add(new Sprzet(3,"Salomon",29.99,"Buty Narciarskie",true));
-        listaSprzetow.add(new Sprzet(4,"Salomon",35.00,"Buty Snowboardowe",true));
-        listaSprzetow.add(new Sprzet(5,"Salomon",60.00,"Snowboard",true));
-        listaSprzetow.add(new Sprzet(6,"Rossignol",29.99,"Kask",true));
-        listaSprzetow.add(new Sprzet(7,"Rossignol",45.00,"Narty",true));
-        listaSprzetow.add(new Sprzet(8,"Rossignol",24.99,"Buty Narciarskie",true));
-        listaSprzetow.add(new Sprzet(9,"Rossignol",14.99,"Kijki",true));
+        listaSprzetow.add(new Sprzet(1,"Salomon",50.00,"Narty",true));
+        listaSprzetow.add(new Sprzet(1,"Salomon",29.99,"Buty Narciarskie",true));
+        listaSprzetow.add(new Sprzet(1,"Salomon",35.00,"Buty Snowboardowe",true));
+        listaSprzetow.add(new Sprzet(1,"Salomon",60.00,"Snowboard",true));
+        listaSprzetow.add(new Sprzet(1,"Rossignol",29.99,"Kask",true));
+        listaSprzetow.add(new Sprzet(1,"Rossignol",45.00,"Narty",true));
+        listaSprzetow.add(new Sprzet(1,"Rossignol",24.99,"Buty Narciarskie",true));
+        listaSprzetow.add(new Sprzet(1,"Rossignol",14.99,"Kijki",true));
 
-        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Hello! Press 'Enter' to continue..");
-//        while (scanner.hasNext()){
-//            System.out.println("'U'=uzytkownik , 'K'= kierownik , 'P'=pracownik");
-//            String wybierzUzytkownika= scanner.nextLine();
-//            if (wybierzUzytkownika.equals(wybierzUzytkownika)) {
-//                System.out.println("Witaj Kierowniku, co bedziesz robil:");
-//                System.out.println("Dodac nowy sprzet:'1'");
-//                System.out.println("Usunac sprzet: '2'");
-//                System.out.println("Dodac nowego pracownika:'3");
-//                System.out.println("Usunac pracownika:'4");
-//                String wybirzOpcje = scanner.next();
-//                switch (wybirzOpcje){
-//                    case "1":
-//                        kierownik.DodajNowySprzet(listaSprzetow);
-//                        System.out.println("Sprzet dodany!");
-//                        break;
-//                    case "2":
-//                        kierownik.UsunSprzet(listaSprzetow);
-//                        System.out.println("Sprzet usuniety!");
-//                        break;
-//                    case "3":
-//                        kierownik.DodajPracownika(listaPracownikow);
-//                        System.out.println("Pracownik dodany!");
-//                        break;
-//                    case "4":
-//                        kierownik.UsunPracownika(listaPracownikow);
-//                        System.out.println("Pracownik usuniety");
-//                    case "5":
-//                        kierownik.DodajNowegoInstruktora(listaInstruktorow);
-//                        System.out.println("Instruktor dodany!");
-//                        break;
-//                    case "6":
-//                        kierownik.UsunInstruktora(listaInstruktorow);
-//                        System.out.println("Instruktor usuniety" +
-//                                "!");
-//                        break;
-//                }
-//            }
-//
-//        }
+        System.out.println(listaSprzetow.size());
 
 
+
+
+        System.out.println(listaPracownikow);
     }
 }
